4c37e3de9ca4297e9c2df7f8de37bb4079be7786
Frank Smith <73555034+FrankSmith685@users.noreply.github.com>
2025-02-07 18:18:12 -0500

    update

--- app/assets/HeaderNav.jsx
+++ app/assets/HeaderNav.jsx
@@ -179,7 +179,7 @@ const HeaderNav = () => {
                                 <>
                                     <div className='flex flex-col items-center'>
                                         <div className="relative w-full h-20">
-                                        <Image src="/imagenes/LogoPrincipalWhite.png" alt="XadA" layout="fill" objectFit="contain" />
+                                        <Image src="/imagenes/ww.png" alt="ggg" layout="fill" objectFit="contain" />
                                         </div>
 
 
